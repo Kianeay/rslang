6b97c967d900f2ffa93f173fd7ad6456a8ab42b4
Kianeay <70838054+Kianeay@users.noreply.github.com>
2022-02-04 15:21:22 +0300

    init:  start task rs lang

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-unused-vars */
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env'],
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' }, // to inject the result into the DOM as a style block
+          { loader: 'css-modules-typescript-loader' }, // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
+          { loader: 'css-loader', options: { modules: true } }, // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+          { loader: 'sass-loader' }, // to convert SASS to CSS
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.tsx', '.ts', '.css', '.scss'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, ''),
+    },
+    port: 5000,
+    devMiddleware: {
+      publicPath: 'https://localhost:3000/dist/',
+    },
+  },
+};
