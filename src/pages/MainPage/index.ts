0f91173d89865acc341e43a60a78b9bd1ff1aa02
Kianeay <70838054+Kianeay@users.noreply.github.com>
2022-02-21 14:30:33 +0300

    feat: add section about app layout

--- src/pages/MainPage/index.ts
+++ src/pages/MainPage/index.ts
@@ -38,7 +38,7 @@ export default class MainPage {
     const mainText = document.createElement('p');
     mainText.className = 'main__subtitle';
     mainText.textContent =
-      'Memorizing English words can be fun and challenging. Play games, listen to pronunciation, improve your knowledge. With our app, learning is a joy.';
+      'Learning English is much more interesting than it seems. It is enough to learn it along with our exciting games. We will help you fall in love with English! ';
 
     return mainText;
   }
@@ -64,6 +64,72 @@ export default class MainPage {
     return mainWrap;
   }
 
+  private createAbout() {
+    const wrapper = document.createElement('div');
+    wrapper.className = 'main__about';
+
+    const aboutTitle = document.createElement('h2');
+    aboutTitle.className = 'main__about-subtitle';
+    aboutTitle.textContent = 'About';
+
+    wrapper.append(
+      aboutTitle,
+      this.createImageBlock(
+        'textbook',
+        'The electronic textbook consists of six sections. Each section has 30 pages of 20 words. Here you can significantly expand your vocabulary.',
+        'Textbook',
+      ),
+      this.createImageBlock(
+        'games',
+        '2 most popular games to improve your English listening and comprehension skills. Check how much points you can get in one minute, making educated guesses about what is right and what is wrong',
+        'Games',
+      ),
+      this.createImageBlock(
+        'statistics',
+        'Thanks to statistics, you will not only see your progress, but also motivate yourself to better results.',
+        'Statistics',
+      ),
+    );
+
+    return wrapper;
+  }
+
+  private createImageBlock(name: string, text: string, title: string) {
+    const wrapper = document.createElement('div');
+    wrapper.className = 'main__info';
+    if (name === 'games') {
+      wrapper.classList.add('main__reverse');
+    }
+
+    const textWrapper = document.createElement('div');
+    textWrapper.className = 'main__text-wrap';
+
+    const aboutTitle = document.createElement('h2');
+    aboutTitle.className = 'main__about-title';
+    aboutTitle.textContent = title;
+
+    const aboutText = document.createElement('p');
+    aboutText.className = 'main__about-text';
+    aboutText.textContent = text;
+
+    textWrapper.append(aboutTitle, aboutText);
+
+    wrapper.append(this.createIcon(name), textWrapper);
+
+    return wrapper;
+  }
+
+  private createIcon(name: string) {
+    const iconWrap = document.createElement('div');
+    iconWrap.className = 'main__icon-wrap';
+    const svg = document.createElement('img');
+    svg.className = 'main__about-img';
+    svg.src = `src/assets/images/${name}.svg`;
+    iconWrap.append(svg);
+
+    return iconWrap;
+  }
+
   render() {
     const component = document.createElement('div');
     component.className = 'main';
@@ -74,6 +140,7 @@ export default class MainPage {
       this.createMainContent(),
       this.createMainBackground(),
       this.createLoginBtn(),
+      this.createAbout(),
       footer,
     );
 
